4d2e0f4757cd42668a1591942592f369631e417b
Nikhil Patil <nikhil.patil.3632@gmail.com>
2024-11-08 14:34:05 +0100

    Initial commit

--- apps/radiology-reporting-suite/eslint.config.js
+++ apps/radiology-reporting-suite/eslint.config.js
@@ -1,10 +1,16 @@
 const nx = require('@nx/eslint-plugin');
 const baseConfig = require('../../eslint.config.js');
+const importPlugin = require('eslint-plugin-import');
+const rxjsPlugin = require('eslint-plugin-rxjs-x');
+const ngrxPlugin = require('@ngrx/eslint-plugin')
 
 module.exports = [
   ...baseConfig,
   ...nx.configs['flat/angular'],
   ...nx.configs['flat/angular-template'],
+  // ...importPlugin.configs.recommended,
+  // ...rxjsPlugin.configs.recommended,
+  // ...ngrxPlugin.configs.recommended,
   {
     files: ['**/*.ts'],
     rules: {
