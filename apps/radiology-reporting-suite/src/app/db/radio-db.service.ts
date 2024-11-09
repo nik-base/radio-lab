3d5ae578da4470c179f97bfecea2b6bb52e3ad44
Nikhil Patil <nikhil.patil.3632@gmail.com>
2024-11-09 22:38:10 +0100

    Refactor radio db service

--- apps/radiology-reporting-suite/src/app/db/radio-db.service.ts
+++ apps/radiology-reporting-suite/src/app/db/radio-db.service.ts
@@ -16,10 +16,12 @@ import {
   RadioTemplateImportModelDto,
   RadioTemplateModelDto,
   RadioTemplateUpdateRequestDto,
+  RTFEditorContentDto,
 } from '../models/data';
 
 import {
   RadioFindingAreaDBModel,
+  RadioFindingDetailsDBModel,
   RadioTemplateDBModel,
 } from './radio-db.models';
 
@@ -29,26 +31,16 @@ export class RadioDBService {
     inject(NgxIndexedDBService);
 
   fetchTemplates$(): Observable<RadioTemplateDto[]> {
-    return this.radioDBService.getAll<RadioTemplateDBModel>('templates').pipe(
-      map((templates: RadioTemplateDBModel[]) =>
-        templates.map(
-          (template: RadioTemplateDBModel): RadioTemplateDto => ({
-            id: template.id,
-            name: template.name,
-            protocol: {
-              text: template.description ?? '',
-              html: template.decriptionHTML ?? '',
-              json: template.decriptionJSON ?? '',
-            },
-            patientInfo: {
-              text: template.patientInfo ?? '',
-              html: template.patientInfoHTML ?? '',
-              json: template.patientInfoJSON ?? '',
-            },
-          })
+    return this.radioDBService
+      .getAll<RadioTemplateDBModel>('templates')
+      .pipe(
+        map((templates: RadioTemplateDBModel[]) =>
+          templates.map(
+            (template: RadioTemplateDBModel): RadioTemplateDto =>
+              this.mapTemplateDBModelToTemplateDto(template)
+          )
         )
-      )
-    );
+      );
   }
 
   fetchFindingAreas$(templateId: string): Observable<RadioFindingAreaDto[]> {
@@ -59,23 +51,34 @@ export class RadioDBService {
         IDBKeyRange.only(templateId)
       )
       .pipe(
-        map((protocols: RadioFindingAreaDBModel[]) => {
-          return protocols.map(
-            (protocol: RadioFindingAreaDBModel): RadioFindingAreaDto => ({
-              id: protocol.id,
-              name: protocol.name,
-              sortOrder: protocol.order,
-              templateId: protocol.templateId,
-            })
-          );
-        })
+        map((findingAreas: RadioFindingAreaDBModel[]) =>
+          findingAreas.map(
+            (findingArea: RadioFindingAreaDBModel): RadioFindingAreaDto =>
+              this.mapFindingAreasDBModelToFindingAreasDto(findingArea)
+          )
+        )
       );
   }
 
   fetchFindingDetails$(
     findingAreaId: string
   ): Observable<RadioFindingDetailsDto[]> {
-    throw new Error('Method not implemented.');
+    return this.radioDBService
+      .getAllByIndex<RadioFindingDetailsDBModel>(
+        'findings',
+        'protocolId',
+        IDBKeyRange.only(findingAreaId)
+      )
+      .pipe(
+        map((findingDetails: RadioFindingDetailsDBModel[]) =>
+          findingDetails.map(
+            (
+              findingDetail: RadioFindingDetailsDBModel
+            ): RadioFindingDetailsDto =>
+              this.mapFindingDetailsDBModelToFindingDetailsDto(findingDetail)
+          )
+        )
+      );
   }
 
   createTemplate$(
@@ -158,5 +161,181 @@ export class RadioDBService {
     findingDetailsId: string
   ): Observable<RadioFindingDetailsDto> {
     throw new Error('Method not implemented.');
+  }
+
+  private mapTemplateDBModelToTemplateDto(
+    template: RadioTemplateDBModel
+  ): RadioTemplateDto {
+    return {
+      id: template.id,
+      name: template.name,
+      protocol:
+        this.mapTemplateProtocolEditorContentToRTFEditorContent(template),
+      patientInfo:
+        this.mapTemplatePatientInfoEditorContentToRTFEditorContent(template),
+    };
+  }
+
+  private mapFindingAreasDBModelToFindingAreasDto(
+    findingArea: RadioFindingAreaDBModel
+  ): RadioFindingAreaDto {
+    return {
+      id: findingArea.id,
+      name: findingArea.name,
+      sortOrder: findingArea.order,
+      templateId: findingArea.templateId,
+    };
+  }
+
+  private mapFindingDetailsDBModelToFindingDetailsDto(
+    findingDetail: RadioFindingDetailsDBModel
+  ): RadioFindingDetailsDto {
+    return {
+      id: findingDetail.id,
+      name: findingDetail.title,
+      group: findingDetail.group ?? null,
+      isNormal: findingDetail.isNormal ?? false,
+      sortOrder: findingDetail.order ?? 0,
+      findingAreaId: findingDetail.protocolId,
+      description:
+        this.mapFindingDetailsDescriptionEditorContentToRTFEditorContent(
+          findingDetail
+        ),
+      impression:
+        this.mapFindingDetailsImpressionEditorContentToRTFEditorContent(
+          findingDetail
+        ),
+      recommendation:
+        this.mapFindingDetailsRecommendationEditorContentToRTFEditorContent(
+          findingDetail
+        ),
+    };
+  }
+
+  private mapTemplateProtocolEditorContentToRTFEditorContent(
+    editorContent: RadioTemplateDBModel
+  ): RTFEditorContentDto {
+    return {
+      text: editorContent.description ?? '',
+      html: editorContent.decriptionHTML ?? '',
+      json: editorContent.decriptionJSON ?? '',
+    };
+  }
+
+  private mapRTFEditorContentToTemplateProtocolEditorContent(
+    editorContent: RTFEditorContentDto
+  ): Pick<
+    RadioTemplateDBModel,
+    'description' | 'decriptionHTML' | 'decriptionJSON'
+  > {
+    return {
+      description: editorContent.text,
+      decriptionHTML: editorContent.html,
+      decriptionJSON: editorContent.json,
+    };
+  }
+
+  private mapTemplatePatientInfoEditorContentToRTFEditorContent(
+    editorContent: RadioTemplateDBModel
+  ): RTFEditorContentDto | null {
+    if (!editorContent.patientInfoHTML) {
+      return null;
+    }
+
+    return {
+      text: editorContent.patientInfo ?? '',
+      html: editorContent.patientInfoHTML ?? '',
+      json: editorContent.patientInfoJSON ?? '',
+    };
+  }
+
+  private mapRTFEditorContentToTemplatePatientInfoEditorContent(
+    editorContent: RTFEditorContentDto
+  ): Pick<
+    RadioTemplateDBModel,
+    'patientInfo' | 'patientInfoHTML' | 'patientInfoJSON'
+  > {
+    return {
+      patientInfo: editorContent.text,
+      patientInfoHTML: editorContent.html,
+      patientInfoJSON: editorContent.json,
+    };
+  }
+
+  private mapFindingDetailsDescriptionEditorContentToRTFEditorContent(
+    editorContent: RadioFindingDetailsDBModel
+  ): RTFEditorContentDto {
+    return {
+      text: editorContent.description ?? '',
+      html: editorContent.decriptionHTML ?? '',
+      json: editorContent.decriptionJSON ?? '',
+    };
+  }
+
+  private mapRTFEditorContentToFindingDetailsDescriptionEditorContent(
+    editorContent: RTFEditorContentDto
+  ): Pick<
+    RadioFindingDetailsDBModel,
+    'description' | 'decriptionHTML' | 'decriptionJSON'
+  > {
+    return {
+      description: editorContent.text,
+      decriptionHTML: editorContent.html,
+      decriptionJSON: editorContent.json,
+    };
+  }
+
+  private mapFindingDetailsImpressionEditorContentToRTFEditorContent(
+    editorContent: RadioFindingDetailsDBModel
+  ): RTFEditorContentDto | null {
+    if (!editorContent.impressionHTML) {
+      return null;
+    }
+
+    return {
+      text: editorContent.impression ?? '',
+      html: editorContent.impressionHTML ?? '',
+      json: editorContent.impressionJSON ?? '',
+    };
+  }
+
+  private mapRTFEditorContentToFindingDetailsImpressionEditorContent(
+    editorContent: RTFEditorContentDto
+  ): Pick<
+    RadioFindingDetailsDBModel,
+    'impression' | 'impressionHTML' | 'impressionJSON'
+  > {
+    return {
+      impression: editorContent.text,
+      impressionHTML: editorContent.html,
+      impressionJSON: editorContent.json,
+    };
+  }
+
+  private mapFindingDetailsRecommendationEditorContentToRTFEditorContent(
+    editorContent: RadioFindingDetailsDBModel
+  ): RTFEditorContentDto | null {
+    if (!editorContent.recommendationHTML) {
+      return null;
+    }
+
+    return {
+      text: editorContent.recommendation ?? '',
+      html: editorContent.recommendationHTML ?? '',
+      json: editorContent.recommendationJSON ?? '',
+    };
+  }
+
+  private mapRTFEditorContentToFindingDetailsRecommendationEditorContent(
+    editorContent: RTFEditorContentDto
+  ): Pick<
+    RadioFindingDetailsDBModel,
+    'recommendation' | 'recommendationHTML' | 'recommendationJSON'
+  > {
+    return {
+      recommendation: editorContent.text,
+      recommendationHTML: editorContent.html,
+      recommendationJSON: editorContent.json,
+    };
   }
 }
