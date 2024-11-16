42484f64620aec290fe1666155e59eeef14f02f2
Nikhil Patil <nikhil.patil.3632@gmail.com>
2024-11-16 22:33:27 +0100

    Ignored css parsing error in tests

--- apps/radiology-reporting-suite/src/test-setup.ts
+++ apps/radiology-reporting-suite/src/test-setup.ts
@@ -6,3 +6,26 @@ globalThis.ngJest = {
   },
 };
 import 'jest-preset-angular/setup-jest';
+
+/*
+  Referred from https://github.com/jsdom/jsdom/issues/2177#issuecomment-1724971596
+  Ignore CSS parsing error due to known issue with jest and jsDOM
+  Issue links -
+  https://github.com/jsdom/jsdom/issues/2177
+  https://github.com/thymikee/jest-preset-angular/issues/2194
+  https://github.com/primefaces/primeng/issues/14085
+*/
+const originalConsoleError: (...params: unknown[]) => void = console.error;
+
+const jsDomCssError: string = 'Error: Could not parse CSS stylesheet';
+
+console.error = (...params: unknown[]): void => {
+  if (
+    !params.find(
+      (param: unknown): boolean =>
+        param?.toString()?.includes(jsDomCssError) ?? false
+    )
+  ) {
+    originalConsoleError(...params);
+  }
+};
